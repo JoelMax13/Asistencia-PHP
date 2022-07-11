984d09d5db3f6d0aedcb85e5fb69802f53ab0e88
JoelMax13 <joel.ona@hotmail.com>
2022-07-10 19:27:08 -0500

    Diseño Sistema Asistencia
    Diseño Formulario Crear Usuario
    v(0.0.2)

--- Vistas/CrearUsuario.php
+++ Vistas/CrearUsuario.php
@@ -2,42 +2,58 @@
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Asistencia</title>
     <!-- CSS only -->
-    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-    <link rel="stylesheet" href="Index.css"> -->
-    <link href="../Diseño/Sistema.css" type="text/css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link href="../Diseno/Sistema.css" type="text/css" rel="stylesheet">
 </head>
+
 <body>
-<form action="Registrar" method="POST">
-    <div>
-    <label class="RegDerecha">Cedula:</label>
-    <Input type="text" class="RegIzquierda" name="Cedula"></Input>
-    </div>
-    <div>
-    <label class="RegDerecha">Nombres Apellidos:</label>
-    <Input type="text" class="RegIzquierda" name="NombresApellidos"></Input>
-    </div>
-    <div>
-    <label class="RegDerecha">Teléfono:</label>
-    <Input type="text" class="RegIzquierda" name="Telf"></Input>
-    </div>
-    <div>
-    <label class="RegDerecha">Cargo:</label>
-    <Input type="text" class="RegIzquierda" name="Cargo"></Input>
-    </div>
-    <div>
-    <label class="RegDerecha">Jornada:</label>
-    <Input type="text" class="RegIzquierda" name="Jornada"></Input>
-    </div>
-    <div>
-    <label class="RegDerecha">Proyecto:</label>
-    <Input type="text" class="RegIzquierda" name="Proyecto"></Input>
-    </div>
-</form>
+    <form action="Registrar" method="POST">
+        <div class="container">
+            <div class="mb-3 row">
+                <label class="col-sm-2 col-form-label">Cedula: </label>
+                <div class="col-sm-10">
+                    <input type="text" class="form-control" name="Cedula"/>
+                </div>
+            </div>
+            <div class="mb-3 row">
+                <label class="col-sm-2 col-form-label">Nombres Apellidos:</label>
+                <div class="col-sm-10">
+                    <input type="text" class="form-control" name="NombresApellidos"/>
+                </div>
+            </div>
+            <div class="mb-3 row">
+                <label class="col-sm-2 col-form-label">Teléfono:</label>
+                <div class="col-sm-10">
+                    <input type="text" class="form-control" name="Telf"/>
+                </div>
+            </div>
+            <div class="mb-3 row">
+                <label class="col-sm-2 col-form-label">Cargo:</label>
+                <div class="col-sm-10">
+                    <input type="text" class="form-control" name="Cargo"/>
+                </div>
+            </div>
+            <div class="mb-3 row">
+                <label class="col-sm-2 col-form-label">Jornada:</label>
+                <div class="col-sm-10">
+                    <input type="text" class="form-control" name="Jornada"/>
+                </div>
+            </div>
+            <div class="mb-3 row">
+                <label class="col-sm-2 col-form-label">Proyecto:</label>
+                <div class="col-sm-10">
+                    <input type="text" class="form-control" name="Proyecto"/>
+                </div>
+            </div>
+        </div>
+    </form>
 </body>
+
 </html>
