d475ea87f3e6abd0b10eeec9511d070442c332d4
JoelMax13 <72949289+JoelMax13@users.noreply.github.com>
2022-07-25 10:22:10 -0500

    Programación Sistema Asistencia
    Registro de asistencias con parámatros
    v(0.4.0)

--- Controladores/CrearConexion.php
+++ Controladores/CrearConexion.php
@@ -1,10 +1,15 @@
 <?php
+date_default_timezone_set('America/Guayaquil');
 session_start();
-    $_SESSION['registrador'] = "Luis Joel Oña Méndez";
+    $_SESSION['id'] = 8;
+    $_SESSION['registrador'] = "LUIS JOEL OÑA MÉNDEZ";
+    $_SESSION['fechaActual'] = date('Y-m-d'); 
+    $_SESSION['horaActual'] = date('H:i:s');
+
     $usuario = "root";
     $contrasena = "";
     $server = "localhost";
-    $db = "asistenciatectotal";
+    $db = "asistencia_tectotal";
     //Puerto Oficina
     $puerto = "3306";
     //  Puerto Casa
@@ -15,5 +20,5 @@ session_start();
     if(!$conexion){
         echo "Error en la conexión con la base de datos.";
     }
+    //session_destroy();
 ?>
-//session_destroy();
--- Controladores/RegistrarAsistencia.php
+++ Controladores/RegistrarAsistencia.php
@@ -1,3 +1,38 @@
 <?php
+    include ("../Controladores/CrearConexion.php");
+    $PRO_ID = $_POST['proyecto'];
+    $AsistenciaRegistrada = GuardarAsistencia($conexion, $PRO_ID);
+    if($AsistenciaRegistrada){
+        echo '<script language="javascript">alert("Datos guardados correctamente!");window.location.href="../Vistas/Inicio.php"</script>';
+    }else{
+        echo '<script language="javascript">alert("No se pudo ingresar los datos");window.location.href="../Vistas/Inicio.php"</script>';
+    }
+    
+    function GuardarAsistencia($conexion, $PRO_ID){
+        $retorno = false;
+        $Select_UsPr = "SELECT USPR_ID FROM tbl_usuario_proyecto 
+                    WHERE (PRO_ID = '{$PRO_ID}'); ";
+        $Resultado = mysqli_query($conexion, $Select_UsPr);
+
+        while($Lista_UsPr = mysqli_fetch_assoc($Resultado)) {
+            //Guardar datos
+            $UsPr_ID = $Lista_UsPr['USPR_ID'];
+            if(isset($_POST['valor'.$UsPr_ID])){
+                $ASIS_VERIFICACION = $_POST['valor'.$UsPr_ID];
+                $Insert_Asistencia = "INSERT INTO tbl_asistencia (USPR_ID, ASIS_FECHA, ASIS_REGISTRADOR, ASIS_VERIFICACION)
+                                VALUES ('{$UsPr_ID}','{$_SESSION['fechaActual']}','{$_SESSION['registrador']}','{$ASIS_VERIFICACION}')";
+                $AsistenciaIngresada = mysqli_query($conexion, $Insert_Asistencia);
+                if($AsistenciaIngresada){
+                    $retorno = true;
+                }else{
+                    $retorno = false;
+                }
+            }else{
+                $retorno = true;
+            }
+        }
+        return $retorno;
+    }
+
     
 ?>
