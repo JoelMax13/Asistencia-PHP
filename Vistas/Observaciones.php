f00f257a01fdab5dcf581ad6af9906682d9565ba
JoelMax13 <72949289+JoelMax13@users.noreply.github.com>
2022-07-25 12:27:47 -0500

    Programación Sistema Asistencia
    Inicio de automatización del apartado de observaciones
    v(0.4.1)

--- Vistas/Observaciones.php
+++ Vistas/Observaciones.php
@@ -1,3 +1,8 @@
+<?php
+        include ("../Controladores/CrearConexion.php");
+        $Pro_Nombre = $_GET['nombre'];
+        $PRO_ID = ObtenerIdProyecto($conexion, $Pro_Nombre);
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -8,23 +13,31 @@
     <title>Observaciones</title>
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
     <link href="../Diseno/Sistema.css" type="text/css" rel="stylesheet">
 </head>
 
 <body>
-    <nav>
+    <?php
+        include ("MenuPrincipal.php");
+    ?>
+
+    <div class="body">
+    <div>
         <h1>Observaciones</h1>
         <label class="infoBasic">
             <strong>Fecha:</strong>
             <?php
-                $Date = date('m-d-Y');  
-                echo $Date;
+                echo $_SESSION['fechaActual'];
             ?>
         </label>
         <label class="infoBasic">
             <strong>Personal a cargo:</strong>
+            <?php
+                echo $_SESSION['registrador'];
+            ?>
         </label>
-    </nav>
+    </div>
     <form action="../Controladores/RegistrarObservaciones.php" method="POST">
         <div class="container O_container">
             <!-- Mostrar Lista de personas -->
@@ -34,47 +47,73 @@
                     <label class="col-sm-4"><strong>Comentario</strong></label>
                     <label class="col-sm-4"><strong>Observación</strong></label>
                 </li>
-                <li class="mb-3 row">
-                    <label class="col-sm-4 col-form-label">Luis Andrés Naranjo Rodriguez</label>
-                    <div class="col-sm-3">
-                        <!-- ***************************************************************************** -->
-                        <select class="form-select">
-                            <option value="">---</option>
-                            <option value="">Descanso de jornada</option>
-                            <option value="">Falta justificada</option>
-                            <option value="">Falta injustificada</option>
-                            <option value="">Jornada nocturna</option>
-                            <option value="">Días sobre jornada</option>
-                            <option value="">Liquidación</option>
-                        </select>
-                    </div>
-                    <div class="col-sm-5">
-                        <!-- <textarea class="form-control" aria-label="With textarea"></textarea> -->
-                        <input class="form-control"></input>
-                    </div>
-                </li>
-                <li class="mb-3 row">
-                    <label class="col-sm-4 col-form-label">Luis Andrés Naranjo Rodriguez</label>
-                    <div class="col-sm-3">
-                    <select class="form-select">
-                            <option value="">---</option>
-                            <option value="">Descanso de jornada</option>
-                            <option value="">Falta justificada</option>
-                            <option value="">Falta injustificada</option>
-                            <option value="">Jornada nocturna</option>
-                            <option value="">Días sobre jornada</option>
-                            <option value="">Liquidación</option>
-                        </select>
-                    </div>
-                    <div class="col-sm-5">
-                        <!-- <textarea class="form-control" aria-label="With textarea"></textarea> -->
-                        <input class="form-control"></input>
-                    </div>
-                </li>
+                
+
+                    <?php
+                        $Select_UsuariosProyecto = "SELECT USPR.USPR_ID, USU_NOMAPE FROM `tbl_usuario` as USU 
+                        LEFT JOIN tbl_usuario_proyecto as USPR 
+                        ON (USU.USU_ID = USPR.USU_ID) WHERE (USPR.PRO_ID = '{$PRO_ID}'); ";
+                        $Arr_UsuariosProyecto = mysqli_query($conexion, $Select_UsuariosProyecto);
+                        if (mysqli_num_rows($Arr_UsuariosProyecto) > 0) {
+                            while($conjuntoDatos = mysqli_fetch_assoc($Arr_UsuariosProyecto)) {
+                                $USPR_ID = $conjuntoDatos['USPR_ID'];
+                                $USU_NOMAPE = $conjuntoDatos['USU_NOMAPE'];
+                                VerificarAsistencia($conexion, $USPR_ID, $USU_NOMAPE);
+                            }
+                            $mostrarBoton = 'active';
+                        } else {
+                            echo "0 results";
+                            $mostrarBoton = 'disabled';
+                        }
+                    ?>
+                
             </ul>
             <button type="submit" class="btn btn-success BtnFinProc">Guardar</button>
         </div>
     </form>
+    </div>
 </body>
-
+<?php
+    function ObtenerIdProyecto($conexion, $proyecto){
+        //Obtener Id Proyecto
+        $Select_ProId = "Select PRO_ID from tbl_proyecto where (PRO_NOMBRE = '{$proyecto}')";
+        $OBJ_PRO_ID = mysqli_query($conexion, $Select_ProId);
+        $ARR_PRO_ID = mysqli_fetch_assoc($OBJ_PRO_ID);
+        $PRO_ID = implode($ARR_PRO_ID);
+        return $PRO_ID;
+    }
+    function VerificarAsistencia($conexion, $UsPr_ID, $USU_NOMAPE){
+        $Select_ConRegistro = "SELECT USPR_ID FROM tbl_asistencia 
+                                WHERE ((USPR_ID = '{$UsPr_ID}')AND(ASIS_FECHA = '{$_SESSION['fechaActual']}')AND(ASIS_VERIFICACION = 1)); ";
+        $RegistradoConAsis = mysqli_query($conexion, $Select_ConRegistro);
+        $Registrado = mysqli_fetch_assoc($RegistradoConAsis);
+        if($Registrado == NULL){
+            echo 
+            '<li class="mb-3 row">
+                <label class="col-sm-4 col-form-label">'.$USU_NOMAPE.'</label>
+                <div class="col-sm-3"> 
+                        <select class="form-select">';
+            VerListaObservaciones($conexion);
+            $Select_AsisId = "SELECT ASIS_ID FROM tbl_asistencia WHERE (USPR_ID = '{$UsPr_ID}')";
+            $Resultado_AsisId = mysqli_query($conexion, $Select_AsisId);
+            $ASIS_ID = mysqli_fetch_assoc($Resultado_AsisId);
+            echo '</select>
+                    </div>
+                    <div class="col-sm-5">
+                        <input name="obs'.$ASIS_ID['ASIS_ID'].'" class="form-control"></input>
+                    </div>
+            </li>';
+        }
+    }
+    function VerListaObservaciones($conexion){
+        $Select_Observaciones = "Select OBS_ID, OBS_COMENTARIO from tbl_observacion";
+        $result = mysqli_query($conexion, $Select_Observaciones);
+        while($elemento = mysqli_fetch_assoc($result)) {
+            echo 
+            '<option value="'.$elemento['OBS_ID'].'">'
+                .$elemento['OBS_COMENTARIO'].
+            '</option>';
+        }
+    }
+?>
 </html>
